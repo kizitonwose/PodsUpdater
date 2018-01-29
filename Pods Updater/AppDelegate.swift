a117520378b6320215954f13a1eece6afc54725d
Kizito Nwose <kizitonwose@gmail.com>
2018-01-29 17:00:05 +0100

    Initial commit.

---
+++ Pods Updater/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pods Updater
+//
+//  Created by Kizito Nwose on 29/01/2018.
+//  Copyright © 2018 Kizito Nwose. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pods Updater/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pods Updater
+//
+//  Created by Kizito Nwose on 29/01/2018.
+//  Copyright © 2018 Kizito Nwose. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
